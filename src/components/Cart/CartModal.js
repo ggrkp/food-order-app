843147c8f8930714b389007b99bf939a141ba8e8
Georgakopoulos Giorgos <ggeorg.prime@gmail.com>
2022-04-11 23:12:02 +0300

    added some visuals

--- src/components/Cart/CartModal.js
+++ src/components/Cart/CartModal.js
@@ -2,36 +2,65 @@ import styles from './CartModal.module.css'
 import ReactDom from 'react-dom'
 // ! εδω πρεπει να εμφανιζονται τα στοιχεια της λιστας αγορων.
 import PrimaryButton from '../UI/PrimaryButton'
-import CartItemList from './CartItemList'
+// import CartItemList from './CartItemList'
+import CartItem from './CartItem'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import { faPaperPlane, faDollarSign } from '@fortawesome/free-solid-svg-icons'
 
 const CartModal = (props) => {
 
+    //  ! dummy cart items 
+    const cartItemsArray = [
+        { name: 'Item 1', price: 100 },
+        { name: 'Item 2', price: 40 },
 
+    ]
+    const itemList = cartItemsArray.map(item => <CartItem item={item} />)
 
     const closeModalHandler = () => {
         props.onCloseModal()
     }
+
     const orderHandler = () => {
         console.log('Ordering...')
     }
+
     const ModalOverlay = (props) => {
         return (
+
             <div id="cart-modal" className={styles["modal"]}>
                 <div className={styles["modal-content"]}>
                     <span onClick={closeModalHandler} className={styles["close"]}>&times;</span>
+                    {/* Modal Title */}
                     <h1 className={styles["modal-title"]}>Your Cart</h1>
-                    <CartItemList />
+
+                    {/* Modal Content - Cart Items */}
+                    {itemList}
+
+                    {/* <CartItemList /> Mporei na ginei kai me ksexwristo component... */}
+
+                    {/* Order Button */}
                     <div className={styles["float-right"]}>
-                        <PrimaryButton buttonText={<>Place Order <FontAwesomeIcon icon={faPaperPlane} /></>} onClick={orderHandler} />
-                        <FontAwesomeIcon icon="fa-solid fa-paper-plane" />
+                        
+                        <div
+                            className={styles["total-amount"]}>
+                            Total Amount: <FontAwesomeIcon icon={faDollarSign} /> 50
+                        </div>
+
+                        <div className={styles["btn-container"]}>
+                            <PrimaryButton
+                                id="order-btn"
+                                buttonText={<>Place Order<FontAwesomeIcon icon={faPaperPlane} /></>}
+                                onClick={orderHandler}
+                            />
+                        </div>
+
                     </div>
                     <div className={styles.clearfix}></div>
 
-                    <footer className={styles["modal-footer"]}>More text on footer section</footer>
                 </div>
             </div>
+
         )
     }
 
